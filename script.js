32de76dae61c9c46ebd91a2e2ea014fba6fcfacc
Shivam <73821061+billabonds@users.noreply.github.com>
2023-06-14 12:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 
-	if(key == sampleObject.){
+	if("key" in sampleObject){
 	console.log(true);
 	}
 	
 	else{
 	console.log(false);
 	}
+ 
 }
 
 // Do not change the code below
